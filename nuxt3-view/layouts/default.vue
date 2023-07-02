aa8f7a36e13ddbab7da3a54a62b9a2e372a5c149
sgmikami03 <sgmikami03@gmail.com>
2023-07-02 12:45:16 +0900

    [s5]layout追加

---
+++ nuxt3-view/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <slot />
+        <footer>&copy; progulus.jp</footer>
+    </div>
+</template>
+
+<style>
+.container {
+    color: red;
+}
+
+footer {
+    background-color: orange;
+    color: white;
+    text-align: center;
+}
+</style>
