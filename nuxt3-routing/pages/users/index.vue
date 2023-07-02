a66cc7e2d222486c466f4f8a2267deeeee5f68d1
sgmikami03 <sgmikami03@gmail.com>
2023-07-02 11:51:12 +0900

    [s4]ルーティングについて

---
+++ nuxt3-routing/pages/users/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>users/index pageです！</h1>
+    </div>
+</template>
---
+++ nuxt3-routing/pages/users/other.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>users/index otherです！</h1>
+    </div>
+</template>
