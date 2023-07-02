af02f7d2903a8de9d1e6e99b73faf5363a46ecda
sgmikami03 <sgmikami03@gmail.com>
2023-07-02 11:13:09 +0900

    [s3] add pages

--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -2,6 +2,6 @@
     <div>
         <h1>Top Page</h1>
         <hr>
-        <NuxtLink to="/price">PricePage</NuxtLink>
+        <NuxtLink to="/price">PricePage</NuxtLink> | <NuxtLink to="/mypage">MyPage</NuxtLink>
     </div>
 </template>
---
+++ nuxt3-app/pages/mypage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Mypage</h1>
+        <hr>
+        <NuxtLink to="/">TopPage</NuxtLink>
+    </div>
+</template>
