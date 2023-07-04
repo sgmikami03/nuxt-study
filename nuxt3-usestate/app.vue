fa031a32a2950329fd13b2e61bb8fbbf14bbb94e
sgmikami03 <sgmikami03@gmail.com>
2023-07-04 21:01:48 +0900

    [s8]use state

--- nuxt3-usestate/app.vue
+++ nuxt3-usestate/app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>{{ title }}</p>
+    <button @click="$event => titleState.changeTitle('Hello Nuxt')">Hello Nuxtに変更する！</button>
   </div>
 </template>
+
+<script setup>
+  const titleState = useTitle()
+  const { title } = titleState
+</script>
